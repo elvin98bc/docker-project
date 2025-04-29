308f3ad232997b1a19247e6499d8667b45ba29be
elvin98bc <elvin98bc@gmail.com>
2025-04-29 10:58:26 +0800

    added groovy script

--- script.groovy
+++ script.groovy
@@ -2,14 +2,14 @@
 
 def buildImage() {
     echo "Building Docker Image..."
-    sh "docker build --platform linux/amd64 -t ${env.DockerImageTag} ."
+    sh "docker build --platform linux/amd64 -t ${DockerImageTag} ."
 }
 
 def pushImage() {
     echo "Pushing Image to DockerHub..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push ${env.DockerImageTag}"
+        sh "docker push ${DockerImageTag}"
     }
 }
 
@@ -19,7 +19,7 @@ def deployCompose() {
         sh """
         scp -o StrictHostKeyChecking=no ${DotEnvFile} ${DockerComposeFile} ubuntu@${EC2_IP}:/home/ubuntu
         ssh -o StrictHostKeyChecking=no ubuntu@${EC2_IP} "
-            export DC_IMAGE_NAME=${env.DockerImageTag} && \
+            export DC_IMAGE_NAME=${DockerImageTag} && \
             echo $DC_IMAGE_NAME && \
             docker compose -f /home/ubuntu/${DockerComposeFile} --env-file /home/ubuntu/${DotEnvFile} down && \
             docker compose -f /home/ubuntu/${DockerComposeFile} --env-file /home/ubuntu/${DotEnvFile} up -d
