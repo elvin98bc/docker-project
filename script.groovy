81707e4fe47cb070810409a434cfa78dd56c06aa
elvin98bc <elvin98bc@gmail.com>
2025-04-29 10:33:18 +0800

    added groovy script

--- script.groovy
+++ script.groovy
@@ -3,7 +3,7 @@
 def buildImage() {
     echo "Building Docker Image..."
     sh "docker build --platform linux/amd64 -t ${ImageRegistry}/${JOB_NAME}:${BUILD_NUMBER} ."
-    writeFile file: '.env', text: "BUILD_NUMBER=${BUILD_NUMBER}\n"
+    sh "echo BUILD_NUMBER=${BUILD_NUMBER} >> .env"
 }
 
 def pushImage() {
